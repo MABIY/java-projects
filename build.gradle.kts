fd2fee8d5af7552204c0721d8c9291e15e8dbbd7
liu.hua <liu.hua@tineco.com>
2025-06-10 22:14:44 +0800

    build all composite project

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,5 +17,11 @@ repositories {
 }
 
 dependencies {
-    implementation("com.lh.think-in-java:thinking-in-java")
+//    implementation("com.lh.think-in-java:thinking-in-java")
+}
+tasks.named("build") {
+    // 对每个 included build 注册依赖
+    gradle.includedBuilds.map { included ->
+        dependsOn(included.task(":build"))
+    }
 }
