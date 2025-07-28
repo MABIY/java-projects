bf923c964e0308cc85934e8e1d0bfab52f38c312
liu.hua <liu.hua@tineco.com>
2025-07-28 21:45:54 +0800

    ..

--- books/thinking-in-java/src/main/java/chapter3/BitManipulation.java
+++ books/thinking-in-java/src/main/java/chapter3/BitManipulation.java
@@ -13,7 +13,45 @@ public class BitManipulation {
         Random rand = new Random(47);
         int i = rand.nextInt();
         int j = rand.nextInt();
-        //todo
+        printBinaryInt("-1", -1);
+        printBinaryInt("+1", +1);
+        int maxpos = 2147483647;
+        printBinaryInt("maxpos",maxpos);
+        int maxneg = -2147483648;
+        printBinaryInt("maxneg",maxneg);
+        printBinaryInt("i", i);
+        printBinaryInt("~i", ~i);
+        printBinaryInt("-i", -i);
+        printBinaryInt("j", j);
+        printBinaryInt("i & j", i & j);
+        printBinaryInt("i | j", i | j);
+        printBinaryInt("i ^ j", i ^ j);
+        printBinaryInt("i << 5",i << 5);
+        printBinaryInt("i >> 5",i >> 5);
+        printBinaryInt("(~i) >> 5", (~i) >> 5);
+        printBinaryInt("i >>> 5", i >>> 5);
+        printBinaryInt("(~i) >>> 5", (~i) >>> 5);
+
+        long l = rand.nextLong();
+        long m = rand.nextLong();
+        printBinaryLong("-1L", -1L);
+        printBinaryLong("+1L", +1L);
+        long ll = 9223372036854775807L;
+        printBinaryLong("maxpos", ll);
+        long lln = -9223372036854775808L;
+        printBinaryLong("maxneg",lln);
+        printBinaryLong("l",l);
+        printBinaryLong("~l",~l);
+        printBinaryLong("-l",-l);
+        printBinaryLong("m",m);
+        printBinaryLong("l & m",l & m);
+        printBinaryLong("l | m",l | m);
+        printBinaryLong("l ^ m",l ^ m);
+        printBinaryLong("l << 5",l << 5);
+        printBinaryLong("l >> 5",l >> 5);
+        printBinaryLong("(~l) >> 5",(~l) >> 5);
+        printBinaryLong("l >>> 5",l >>> 5);
+        printBinaryLong("(~l) >>> 5",(~l) >>> 5);
     }
 
     static void printBinaryInt(String s, int i) {
@@ -22,7 +60,7 @@ public class BitManipulation {
     }
 
     static void printBinaryLong(String s,long l) {
-        print(s + ", int: " + l + ". binary:" + System.lineSeparator()
+        print(s + ", long: " + l + ". binary:" + System.lineSeparator()
                 + Long.toBinaryString(l));
     }
 }
