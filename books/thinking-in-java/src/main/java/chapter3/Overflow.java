786800c061fb98a152f0c2f88d84a669163101bc
liu.hua <liu.hua@tineco.com>
2025-08-03 21:07:33 +0800

    ..

---
+++ books/thinking-in-java/src/main/java/chapter3/Overflow.java
@@ -0,0 +1,14 @@
+package chapter3;
+
+/**
+ * Surprise! java lets you overflow.
+ * @author lh
+ */
+public class Overflow {
+    public static void main(String[] args) {
+        int big = Integer.MAX_VALUE;
+        System.out.println("big = " + big);
+        int bigger = big * 4;
+        System.out.println("bigger = " + bigger);
+    }
+}
---
+++ books/thinking-in-java/src/main/java/chapter3/practice/StringCompare_po3_14.java
@@ -0,0 +1,32 @@
+package chapter3.practice;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * Write a method that task two string arguments uses all the boolean
+ * comparisons to compare to the two strings and print the results. For the == and
+ * !=, also perform the equals() test. In main(), test your method with some
+ * different String objects.
+ * @author lh
+ */
+public class StringCompare_po3_14 {
+    static void f(boolean b) {
+        if (b == true) {
+            print(true);
+        }else {
+            print(false);
+        }
+    }
+
+    static void stringTest(String s, String t) {
+        f(s == t);
+        f(s.equals(t));
+        f(t.equals(s));
+        f(s != t);
+    }
+
+    public static void main(String[] args) {
+        String s = "one", t = "two";
+        stringTest(s, t);
+    }
+}
