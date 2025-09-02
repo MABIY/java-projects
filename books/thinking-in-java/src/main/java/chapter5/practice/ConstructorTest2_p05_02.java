f3599ce03a10db63518f3827bf870ea92c9b851f
liu.hua <liu.hua@tineco.com>
2025-09-02 20:09:26 +0800

    ..

--- books/thinking-in-java/src/main/java/chapter5/practice/ConstructorTest2_p05_02.java
+++ books/thinking-in-java/src/main/java/chapter5/practice/ConstructorTest2_p05_02.java
@@ -3,20 +3,23 @@ package chapter5.practice;
 /**
  * @author liu.hua
  */
-class Tester2{
+class Tester2 {
     String s1;
     String s2 = "hello";
     String s3;
+
     Tester2() {
         s3 = "good-bye";
     }
 
 
 }
+
 /**
  * @author liu.hua
  */
-public class ConstructorTest2_p05_02 { public static void main(String[] args) {
+public class ConstructorTest2_p05_02 {
+    public static void main(String[] args) {
         Tester2 t = new Tester2();
         System.out.println("t.s1:" + t.s1);
         System.out.println("t.s2:" + t.s2);
